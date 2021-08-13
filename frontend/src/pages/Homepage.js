0a42fc0e91b4f8364dd7ddfb87f955bfd6240df0
Akhil Juluru <akhil.juluru@gmail.com>
2021-08-13 10:53:40 +0530

    first commit

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarComponent from '../components/Navbar'
+
+function Homepage() {
+  return (
+    <div>
+      <NavbarComponent />
+    </div>
+  )
+}
+
+export default Homepage
